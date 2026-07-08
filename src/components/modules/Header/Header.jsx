2cb02364aff7502259ccc57b56e3ec718b2520e2
Nima Zamani <zamani.nima18@gmail.com>
2026-07-08 20:49:49 +0330

    feat: added blurry background for header in desktop.

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -6,7 +6,8 @@ import { navLinks } from '@/data';
 export default function Header() {
   return (
     <header className="sticky top-0 left-0 z-10 w-full lg:fixed">
-      <div className="mx-auto flex max-w-360 items-center justify-between gap-4 bg-white p-4 md:px-10 md:py-5 lg:bg-transparent lg:px-16 lg:pt-10 lg:pb-0">
+      <div className="absolute inset-0 hidden size-full mask-[linear-gradient(to_bottom,black_0%,black_70%,transparent_100%)] backdrop-blur-[5px] lg:block"></div>
+      <div className="relative z-1 mx-auto flex max-w-360 items-center justify-between gap-4 bg-white p-4 md:px-10 md:py-5 lg:bg-transparent lg:px-16 lg:pt-6 lg:pb-5">
         <div className="flex items-center gap-14">
           <div className="flex items-center gap-3">
             <span className="bg-primary size-2 rounded-full lg:bg-white"></span>
