50ffd1b5c94fb3402f714739afc22d6d2172cb51
Nima Zamani <zamani.nima18@gmail.com>
2026-07-10 19:44:17 +0330

    feat: implemented hover effect to services card.

---
+++ src/components/modules/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+export default function ServiceCard({ id, image, title, description }) {
+  return (
+    <div
+      key={id}
+      className="group h-100 max-h-130 origin-center bg-cover bg-center transition-all duration-500 ease-out md:h-112.5"
+      style={{ backgroundImage: `url(/images/services/${image})` }}
+      id="serviceCard"
+    >
+      <Link
+        href=""
+        className="container flex size-full flex-col justify-between gap-4 px-6 pt-4 pb-6"
+      >
+        <h3 className="font-secondary line-clamp-2 h-24 text-[32px] text-white">{title}</h3>
+        <div className="row flex xl:grow flex-col justify-end gap-8 xl:gap-0">
+          <p className="flex grow items-center text-sm leading-loose text-white sm:text-base xl:text-sm">
+            {description}
+          </p>
+          <div className="flex items-center gap-4">
+            <span className="size-1 rounded-full bg-white"></span>
+            <span className="font-semibold text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              بیشتر بدانید
+            </span>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/components/templates/index/Services.jsx
+++ src/components/templates/index/Services.jsx
@@ -1,35 +1,64 @@
-import Link from 'next/link';
+'use client';
+import { useRef } from 'react';
 
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+import ServiceCard from '@modules/ServiceCard/ServiceCard';
 import { services } from '@/data';
 
 export default function Services() {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      const cards = gsap.utils.toArray('#serviceCard', container.current);
+      const MIN_HEIGHT = 400;
+      const MAX_HEIGHT = 520;
+      const MAX_DISTANCE = container.current.clientWidth;
+
+      const setters = cards.map((card) =>
+        gsap.quickTo(card, 'height', {
+          duration: 0.35,
+          ease: 'power2.out',
+        })
+      );
+
+      const handleMove = (e) => {
+        cards.forEach((card, index) => {
+          const rect = card.getBoundingClientRect();
+          const centerX = rect.left + rect.width / 2;
+          const distance = Math.abs(e.clientX - centerX);
+
+          let influence = 1 - distance / MAX_DISTANCE;
+          influence = gsap.utils.clamp(0, 1, influence);
+          influence = influence * influence;
+
+          const height = MIN_HEIGHT + influence * (MAX_HEIGHT - MIN_HEIGHT);
+          setters[index](height);
+        });
+      };
+
+      const handleLeave = () => setters.forEach((set) => set(MIN_HEIGHT));
+
+      if (window.innerWidth >= 1280) {
+        container.current.addEventListener('mousemove', handleMove);
+        container.current.addEventListener('mouseleave', handleLeave);
+      }
+
+      return () => {
+        container.current.removeEventListener('mousemove', handleMove);
+        container.current.removeEventListener('mouseleave', handleLeave);
+      };
+    },
+    { scope: container }
+  );
+
   return (
-    <div className="container pt-10 sm:p-0">
-      <div className="grid grid-cols-1 items-start gap-4 md:grid-cols-2 xl:grid-cols-4!">
-        {services.map((service) => (
-          <div
-            key={service.id}
-            className="group bg-cover bg-center"
-            style={{ backgroundImage: `url(/images/services/${service.image})` }}
-          >
-            <Link
-              href=""
-              className="container flex h-100 w-full max-w-140 flex-col justify-between gap-4 bg-black/20 px-6 pt-4 pb-6 transition-all duration-500 lg:h-112.5"
-            >
-              <h3 className="font-secondary text-[32px] text-white">{service.title}</h3>
-              <div className="flex grow flex-col justify-end gap-8">
-                <p className="text-sm leading-loose text-white sm:text-base xl:text-sm">
-                  {service.description}
-                </p>
-                <div className="flex items-center gap-4">
-                  <span className="size-1 rounded-full bg-white"></span>
-                  <span className="font-semibold text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    بیشتر بدانید
-                  </span>
-                </div>
-              </div>
-            </Link>
-          </div>
+    <div ref={container} className="container flex items-center pt-10 sm:p-0">
+      <div className="grid grid-cols-1 items-center gap-4 md:grid-cols-2 xl:h-130 xl:grid-cols-4!">
+        {services.map(({ id, title, description, image }) => (
+          <ServiceCard key={id} id={id} title={title} description={description} image={image} />
         ))}
       </div>
     </div>
