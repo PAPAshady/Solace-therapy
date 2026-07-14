66be6525e733639c4b1a192fd4e63d7eee816d7d
Nima Zamani <zamani.nima18@gmail.com>
2026-07-14 17:47:10 +0330

    feat: added footer for plans section and updated text selection styles.

--- src/components/templates/index/Plans/Plans.jsx
+++ src/components/templates/index/Plans/Plans.jsx
@@ -13,6 +13,17 @@ export default function Plans() {
             <PlanCard />
           </div>
         </div>
+        <div className="flex flex-col gap-9.5 pt-20 md:flex-row md:items-center md:justify-between md:gap-30 md:pt-40">
+          <h2 className="text-[34px] md:w-1/2">
+            حمایتی آگاهانه، بر پایه تجربه و با هدف{' '}
+            <span className="text-primary">ایجاد تغییری ماندگار</span>
+          </h2>
+          <p className="text-paragraph md:w-1/2">
+            جلسات درمانی ما فضایی امن و آرام فراهم می‌کنند تا تغییر، به شکلی طبیعی و ماندگار شکل
+            بگیرد. در طول مسیر، با دقت به نیازهای شما گوش می‌دهیم، هر جا لازم باشد مسیر را شفاف‌تر
+            می‌کنیم و در رسیدن به اهدافی که خودتان انتخاب کرده‌اید، همراهتان هستیم.
+          </p>
+        </div>
       </div>
     </div>
   );
