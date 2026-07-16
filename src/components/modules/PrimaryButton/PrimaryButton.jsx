d7c368830c85aad7a5bca4b25be30aa6dbec7117
Nima Zamani <zamani.nima18@gmail.com>
2026-07-16 21:40:48 +0330

    fix: adjusted font-size for buttons in mobile.

--- src/components/modules/PrimaryButton/PrimaryButton.jsx
+++ src/components/modules/PrimaryButton/PrimaryButton.jsx
@@ -20,7 +20,7 @@ export default function PrimaryButton({ children, variant, href = '', className,
     <Link
       href={href}
       className={clsx(
-        'font-primary bg-primary group lg:group-data-[theme=light]/header:hover:text-primary lg:group-data-[theme=dark]/header:bg-primary relative flex w-max overflow-hidden rounded-full py-2 ps-12 pe-4 text-sm text-white transition-all duration-300 hover:ps-4 hover:pe-12 lg:text-base lg:group-data-[theme=dark]/header:text-white lg:group-data-[theme=light]/header:bg-white lg:group-data-[theme=light]/header:text-black',
+        'font-primary bg-primary group lg:group-data-[theme=light]/header:hover:text-primary lg:group-data-[theme=dark]/header:bg-primary relative flex w-max overflow-hidden rounded-full py-2 ps-12 pe-4 text-base text-white transition-all duration-300 hover:ps-4 hover:pe-12 lg:group-data-[theme=dark]/header:text-white lg:group-data-[theme=light]/header:bg-white lg:group-data-[theme=light]/header:text-black',
         variant &&
           (variant === 'primary'
             ? 'bg-primary! text-white!'
