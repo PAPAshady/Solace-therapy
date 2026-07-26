859eb615b619efd5a8149d652fc5f1d852fdfbe8
Nima Zamani <zamani.nima18@gmail.com>
2026-07-26 19:29:27 +0330

    feat: implemented about section in about page.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import Hero from '@templates/shared/Hero';
+import AboutUs from '@templates/About/AboutUs';
 
 export default function page() {
   return (
     <div className="space-y-25 pb-16 lg:space-y-50">
       <Hero />
+      <AboutUs />
     </div>
   );
 }
