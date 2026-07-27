0df2131d056407967cec9a51833739b9ed37f989
Nima Zamani <zamani.nima18@gmail.com>
2026-07-27 21:38:04 +0330

    feat: add quote section to about page.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,7 @@ import Hero from '@templates/shared/Hero';
 import AboutUs from '@templates/About/AboutUs';
 import Founders from '@templates/About/Founders';
 import Team from '@templates/About/Team';
+import Quote from '@templates/About/Quote';
 
 export default function page() {
   return (
@@ -10,6 +11,7 @@ export default function page() {
       <AboutUs />
       <Founders />
       <Team />
+      <Quote />
     </div>
   );
 }
