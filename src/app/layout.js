d4c7080fe8a1fe27fc2c7c120be13b3e1fbf8d58
Nima Zamani <zamani.nima18@gmail.com>
2026-09-18 18:33:30 +0330

    fix: changed google search console verification method.

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,12 @@ import TransitionOverlay from '@/TransitionOverlay';
 export default function RootLayout({ children }) {
   return (
     <html lang="fa-IR" dir="rtl">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="nWzTI1LR2nUBHgBYQZlK-0BGPcyK0C60Q2xEp77lJF8"
+        />
+      </head>
       <SmoothScroll />
       <body className="font-primary font-medium text-black">
         <Header />
